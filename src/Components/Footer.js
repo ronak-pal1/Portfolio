aebf144f7dd1d1fb25f6e2915270e92260236ba0
Ronak Paul <ronakpaul882@gmail.com>
2023-09-01 22:12:31 +0530

    Created the Footer component
    Signed-off-by: Ronak Paul <ronakpaul882@gmail.com>

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="qoute">Made with ❤ by drinking ☕</p>
+            <p className="signature">" Ronak Paul "</p>
+        </footer>
+    );
+}
+
+export default Footer;
